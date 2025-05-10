af36590d148bf4a00e7ef6f0ef933769ab7a5e27
imran65545 <cimran1307@gmail.com>
2025-05-10 18:57:00 +0530

    fixed text color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full p-6 bg-white bg-opacity-90 z-50 shadow-md flex justify-between items-center">
+    <nav className="fixed top-0 left-0 w-full p-6 bg-white text-gray-800 bg-opacity-90 z-50 shadow-md flex justify-between items-center">
       <h1 className="text-xl font-bold">A.Carter</h1>
       <div className="space-x-8 uppercase">
         <Link href="/" className="hover:text-gray-500 transition">Home</Link>
